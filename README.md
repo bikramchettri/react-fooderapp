293a4b0994256e621f8352f2c93b95dfa10e8958
bikramchettri <bikramchettri120@outlook.com>
2021-08-29 13:53:06 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-v4q5pc
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-v4q5pc)
---
+++ src/components/Layout/Header.js
@@ -0,0 +1,16 @@
+import { Fragment } from 'react';
+const Header = () => {
+  return (
+    <Fragment>
+      <header>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </header>
+      <div>
+        <img />
+      </div>
+    </Fragment>
+  );
+};
+
+export default Header;
